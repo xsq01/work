884733e31a6de9c63e03e6ece46c06e265f5e8e4
xsq <1805992019@qq.com>
2024-10-31 14:39:09 +0800

    work-0.1

---
+++ docs/.vuepress/.temp/internal/routes.js
@@ -0,0 +1,6 @@
+export const redirects = JSON.parse("{}")
+
+export const routes = Object.fromEntries([
+  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"D:/vuepress/work/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":""} }],
+  ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"D:/vuepress/work/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
+]);
---
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -0,0 +1,3 @@
+import comp from "D:/vuepress/work/docs/.vuepress/.temp/pages/index.html.vue"
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"Hello VuePress\",\"slug\":\"hello-vuepress\",\"link\":\"#hello-vuepress\",\"children\":[]}],\"git\":{},\"filePathRelative\":\"README.md\"}")
+export { comp, data }
