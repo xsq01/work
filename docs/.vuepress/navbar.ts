14a4be0967e86df8fa8945091cb3836b9fa9e627
xsq <1805992019@qq.com>
2025-02-18 11:35:56 +0800

    版本-3.1-操作系统

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -3,6 +3,7 @@ import {NavItem} from "vuepress/config";
 export default [
     {
         text: "SuShy学习记录",
+        ariaLabel: "/sushy学习记录/",
         items: [
             {text: "git",link: '/sushy学习记录/git/'},
             {text: "redis",link: '/sushy学习记录/redis/'},
